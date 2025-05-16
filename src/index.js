683a7fec83d5eae4ae328cf48d6dd110860aeecf
Amal Saleh <saleh-coder@users.noreply.github.com>
2025-05-16 18:26:56 +0100

    feat: create Pizza component and render inside App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 function App() {
-  return <h1>Hello React!</h1>;
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <Pizza />
+    </div>
+  );
+}
+function Pizza() {
+  return <h2>Pizza</h2>;
 }
 
 // In React 18, the root rendering is done this way:
