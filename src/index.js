214d1964bb9d4601dfc755470c38d428f69a70be
Amal Saleh <saleh-coder@users.noreply.github.com>
2025-05-18 13:06:53 +0100

    feat: replace logical && with ternary operator for clearer fallback UI
    Improves conditional rendering in <Menu /> and <Footer /> by using ternary operator to show fallback content when conditions are unmet.

--- src/index.js
+++ src/index.js
@@ -75,12 +75,14 @@ function Menu() {
     <main className="menu">
       <h2>Our menu</h2>
 
-      {numPizzas > 0 && ( // Avoid rendering 0 when no pizzas
+      {numPizzas > 0 ? ( // Avoid rendering 0 when no pizzas
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
       {/* <Pizza
         name="Pizza Spinaci"
@@ -121,11 +123,15 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
